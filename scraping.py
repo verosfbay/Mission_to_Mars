cd97e19b9532af17a5a1563e276befdc861957da
Veronica Lobkina <95447175+verosfbay@users.noreply.github.com>
2022-05-28 17:24:12 -0700

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,39 +1,56 @@
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-# set up the URL for scraping
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+# Connect to MongoDB
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 # Visit the mars nasa news site
 def mars_news(browser):
-
     url = 'https://redplanetscience.com'
     browser.visit(url)
+
     # Optional delay for loading the page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
 
     # set up the html parser
     html = browser.html
     news_soup = soup(html, 'html.parser')
-        # Add try/except for error handling
+
+    # Add try/except for error handling
     try:
         slide_elem = news_soup.select_one('div.list_text')
-
     # begin scraping
         slide_elem.find('div', class_='content_title')
-
     # Use the parent element to find the first `a` tag and save it as `news_title`
         news_title = slide_elem.find('div', class_='content_title').get_text()
-
     # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
     except AttributeError:
         return None, None
+
 
     return news_title, news_p
 
@@ -41,7 +58,6 @@ def mars_news(browser):
 
 # Visit URL
 def featured_image(browser):
-
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
@@ -53,9 +69,8 @@ def featured_image(browser):
     html = browser.html
     img_soup = soup(html, 'html.parser')
 
-        # Add try/except for error handling
+    # Add try/except for error handling
     try:
-
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
@@ -83,7 +98,10 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
    return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
 
