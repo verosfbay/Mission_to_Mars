85879b7005d23ac79274a428c6eceb0e1ed9d759
Veronica Lobkina <you@example.com>
2022-06-11 18:48:06 -0700

    updates

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
+import os
+from flask import send_from_directory
 
 # set up Flask
 app = Flask(__name__)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
@@ -14,15 +19,15 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
-   mars_data = scraping.scrape_all()
    return render_template("index.html", mars=mars)
 
 # Our next function will set up our scraping route.
 # This route will be the "button" of the web application
 @app.route("/scrape")
+
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = Scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
