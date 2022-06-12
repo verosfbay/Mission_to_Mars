eb0926bba0efc54ee0bd6b6c9f643222cbc67895
Veronica Lobkina <you@example.com>
2022-06-11 18:34:45 -0700

    editing

--- scraping.py
+++ scraping.py
@@ -7,8 +7,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # Connect to MongoDB
 def scrape_all():
-    # adding new dictionary for deliverable 2
-    data = {}
 
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -32,7 +30,7 @@ def scrape_all():
 
 # Visit the mars nasa news site
 def mars_news(browser):
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    url = 'https://redplanetscience.com'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -62,7 +60,7 @@ def mars_news(browser):
 
 # Visit URL
 def featured_image(browser):
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full image button
@@ -72,17 +70,15 @@ def featured_image(browser):
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
-
     # Add try/except for error handling
     try:
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-
     except AttributeError:
         return None
 
     # Use the base URL to create an absolute URL
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
     return img_url
 
@@ -90,33 +86,26 @@ def mars_facts():
 
    try:
         # use 'read_html" to scrape the facts table into a dataframe
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
-
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
    except BaseException:
         return None
-
     # Assign columns and set index of dataframe
    df.columns=['description', 'Mars', 'Earth']
    df.set_index('description', inplace=True)
-
     # Convert dataframe into HTML format, add bootstrap
-   return df.to_html()
+   return df.to_html(classes="table table-striped")
 
 def hemispheres(browser):
-    
     # Visit URL
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
-
     # Parse the resulting html with soup
-    hemi_html = browser.html
-    hemi_soup = soup(hemi_html, 'html.parser')
-
-    # Retrieve all items for hemispheres information
-    items = hemi_soup.find_all('div', class_='item')
-
+    hemisphere_html = browser.html
+    hemisphere_soup = soup(hemisphere_html, 'html.parser')
     # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
+    # Retrieve all items for hemispheres information
+    items = hemisphere_soup.find_all('div', class_='item')
 
     # 3. Write code to retrieve the image urls and titles for each hemisphere.
 
@@ -125,19 +114,22 @@ def hemispheres(browser):
     # Create loop to scrape through all hemisphere information
     for i in range(4):
     #create empty dictionary
-        hemispheres = {}
-        browser.find_by_css('a.product-item h3')[i].click()
-        element = browser.links.find_by_text('Sample').first
-        img_url = element['href']
-        title = browser.find_by_css("h2.title").text
-        
-        # add to dictionary using append
-        hemispheres['img_url'] = img_url
-        hemispheres['title'] = title
-        hemisphere_image_urls.append(hemispheres)
+        hemisphere = {}
+        titles = i.find('h3').text
+        # Link for full image
+        link_reference = i.find('a', class_='itemLink product-item')['href']
+        # Use the base URL to create an absolute URL and browser visit
+        browser.visit(url_main + link_reference)
+        # Parse Data
+        image_html = browser.html
+        image_soup = soup(image_html, 'html.parser')
+        download = image_soup.find('div', class_= 'downloads')
+        img_url = download.find('a')['href']
+        # append list
+        hemisphere['img_url'] = img_url
+        hemisphere['title'] = titles
+        hemisphere_image_urls.append(hemisphere)
         browser.back()
-        
-    # 4. Print the list that holds the dictionary of each image url and title.
     return hemisphere_image_urls
 
 if __name__ == "__main__":
